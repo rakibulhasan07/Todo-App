a5577824fa4bfe136280d636efbcf20b1a2a6001
Rakibul Hasan (Rakib) <92722855+rakibulhasan07@users.noreply.github.com>
2026-03-15 17:02:33 +0600

    u

--- todo.js
+++ todo.js
@@ -1,183 +1,183 @@
-const titleInput = document.querySelector("#title");
-const descriptionInput = document.querySelector("#description");
-const addBtn = document.querySelector("#add-btn");
-const todoTableBody = document.querySelector("#todo-table-body");
-const searchInput = document.querySelector("#search");
+// const titleInput = document.querySelector("#title");
+// const descriptionInput = document.querySelector("#description");
+// const addBtn = document.querySelector("#add-btn");
+// const todoTableBody = document.querySelector("#todo-table-body");
+// const searchInput = document.querySelector("#search");
 
-let editIndex = null;
+// let editIndex = null;
 
-if(!localStorage.getItem("todo")){
-localStorage.setItem("todo",JSON.stringify([]))
-}
+// if(!localStorage.getItem("todo")){
+// localStorage.setItem("todo",JSON.stringify([]))
+// }
 
-function loopTodos(filter=""){
+// function loopTodos(filter=""){
 
-let todos = JSON.parse(localStorage.getItem("todo"))
+// let todos = JSON.parse(localStorage.getItem("todo"))
 
-todoTableBody.innerHTML=""
+// todoTableBody.innerHTML=""
 
-let serial = 1
+// let serial = 1
 
-todos.forEach((item,index)=>{
+// todos.forEach((item,index)=>{
 
-if(item.title.toLowerCase().includes(filter.toLowerCase()) ||
-item.description.toLowerCase().includes(filter.toLowerCase())){
+// if(item.title.toLowerCase().includes(filter.toLowerCase()) ||
+// item.description.toLowerCase().includes(filter.toLowerCase())){
 
-todoTableBody.innerHTML+=`
+// todoTableBody.innerHTML+=`
 
-<tr data-id="${index}">
+// <tr data-id="${index}">
 
-<td>${serial}</td>
+// <td>${serial}</td>
 
-<td class="${item.completed ? 'completed':''}">
-${item.title}
-</td>
+// <td class="${item.completed ? 'completed':''}">
+// ${item.title}
+// </td>
 
-<td class="${item.completed ? 'completed':''}">
-${item.description}
-</td>
+// <td class="${item.completed ? 'completed':''}">
+// ${item.description}
+// </td>
 
-<td>
-<input type="checkbox" class="complete-check" ${item.completed ? "checked":""}>
-</td>
+// <td>
+// <input type="checkbox" class="complete-check" ${item.completed ? "checked":""}>
+// </td>
 
-<td>
+// <td>
 
-<button class="edit-btn">Edit</button>
-<button class="delete-btn">Delete</button>
+// <button class="edit-btn">Edit</button>
+// <button class="delete-btn">Delete</button>
 
-</td>
+// </td>
 
-</tr>
-`
+// </tr>
+// `
 
-serial++
+// serial++
 
-}
+// }
 
-})
+// })
 
-deleteTodo()
-editTodo()
-completeTodo()
+// deleteTodo()
+// editTodo()
+// completeTodo()
 
-}
+// }
 
-addBtn.addEventListener("click",()=>{
+// addBtn.addEventListener("click",()=>{
 
-let title = titleInput.value.trim()
-let desc = descriptionInput.value.trim()
+// let title = titleInput.value.trim()
+// let desc = descriptionInput.value.trim()
 
-if(title==="" || desc===""){
-alert("Enter title & description")
-return
-}
+// if(title==="" || desc===""){
+// alert("Enter title & description")
+// return
+// }
 
-let todos = JSON.parse(localStorage.getItem("todo"))
+// let todos = JSON.parse(localStorage.getItem("todo"))
 
-if(editIndex===null){
+// if(editIndex===null){
 
-todos.push({
-title:title,
-description:desc,
-completed:false
-})
+// todos.push({
+// title:title,
+// description:desc,
+// completed:false
+// })
 
-}else{
+// }else{
 
-todos[editIndex].title=title
-todos[editIndex].description=desc
+// todos[editIndex].title=title
+// todos[editIndex].description=desc
 
-editIndex=null
-addBtn.textContent="Add Todo"
+// editIndex=null
+// addBtn.textContent="Add Todo"
 
-}
+// }
 
-localStorage.setItem("todo",JSON.stringify(todos))
+// localStorage.setItem("todo",JSON.stringify(todos))
 
-titleInput.value=""
-descriptionInput.value=""
+// titleInput.value=""
+// descriptionInput.value=""
 
-loopTodos()
+// loopTodos()
 
-})
+// })
 
 
-function deleteTodo(){
+// function deleteTodo(){
 
-document.querySelectorAll(".delete-btn").forEach(btn=>{
+// document.querySelectorAll(".delete-btn").forEach(btn=>{
 
-btn.addEventListener("click",(e)=>{
+// btn.addEventListener("click",(e)=>{
 
-let index = e.target.closest("tr").dataset.id
+// let index = e.target.closest("tr").dataset.id
 
-let todos = JSON.parse(localStorage.getItem("todo"))
+// let todos = JSON.parse(localStorage.getItem("todo"))
 
-todos.splice(index,1)
+// todos.splice(index,1)
 
-localStorage.setItem("todo",JSON.stringify(todos))
+// localStorage.setItem("todo",JSON.stringify(todos))
 
-loopTodos()
+// loopTodos()
 
-})
+// })
 
-})
+// })
 
-}
+// }
 
 
-function editTodo(){
+// function editTodo(){
 
-document.querySelectorAll(".edit-btn").forEach(btn=>{
+// document.querySelectorAll(".edit-btn").forEach(btn=>{
 
-btn.addEventListener("click",(e)=>{
+// btn.addEventListener("click",(e)=>{
 
-let index = e.target.closest("tr").dataset.id
+// let index = e.target.closest("tr").dataset.id
 
-let todos = JSON.parse(localStorage.getItem("todo"))
+// let todos = JSON.parse(localStorage.getItem("todo"))
 
-titleInput.value = todos[index].title
-descriptionInput.value = todos[index].description
+// titleInput.value = todos[index].title
+// descriptionInput.value = todos[index].description
 
-editIndex=index
+// editIndex=index
 
-addBtn.textContent="Update"
+// addBtn.textContent="Update"
 
-})
+// })
 
-})
+// })
 
-}
+// }
 
 
-function completeTodo(){
+// function completeTodo(){
 
-document.querySelectorAll(".complete-check").forEach(check=>{
+// document.querySelectorAll(".complete-check").forEach(check=>{
 
-check.addEventListener("change",(e)=>{
+// check.addEventListener("change",(e)=>{
 
-let index = e.target.closest("tr").dataset.id
+// let index = e.target.closest("tr").dataset.id
 
-let todos = JSON.parse(localStorage.getItem("todo"))
+// let todos = JSON.parse(localStorage.getItem("todo"))
 
-todos[index].completed = e.target.checked
+// todos[index].completed = e.target.checked
 
-localStorage.setItem("todo",JSON.stringify(todos))
+// localStorage.setItem("todo",JSON.stringify(todos))
 
-loopTodos()
+// loopTodos()
 
-})
+// })
 
-})
+// })
 
-}
+// }
 
 
-searchInput.addEventListener("input",(e)=>{
+// searchInput.addEventListener("input",(e)=>{
 
-loopTodos(e.target.value)
+// loopTodos(e.target.value)
 
-})
+// })
 
 
-loopTodos()
+// loopTodos()
